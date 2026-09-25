e359eb25b4ec55e8cad07ebee53f4f232d643576
Eliz_ztu7 <ulianael123@gmail.com>
2026-09-25 14:45:03 +0300

    Більш читабельне тестування в main

--- 25.09.2026_OOP_2/Source.cpp
+++ 25.09.2026_OOP_2/Source.cpp
@@ -68,23 +68,32 @@ int Reservoir::Count = 0;
 
 int main()
 {
+	cout << "===== RESERVOIR 1 =====\n";
 	Reservoir r1;
 	r1.printAll();
+	cout << "--- SETTERS ---\n";
 	r1.setName("NAME");
 	r1.setWidth(10);
 	r1.setLength(20);
 	r1.setMaxDepth(15);
 	r1.printAll();
 
+	cout << "\n===== RESERVOIR 1 =====\n";
 	Reservoir r2("SECOND name", 30, 15, 25);
 	r2.printAll();
+	cout << "--- COMPARE SURFACES #1 ---\n";
 	r2.compareSurfaces(r1);
+	cout << "--- GETTERS ---\n";
 	cout << r2.getName() << endl << r2.getWidth() << endl << r2.getLength() << endl << r2.getMaxDepth() << endl << endl;
 
+	cout << "\n===== RESERVOIR 1 =====\n";
 	Reservoir r3("THIRD name", 10, 20, 40);
 	r3.printAll();
+	cout << "--- COMPARE SURFACES #2 ---\n";
 	r3.compareSurfaces(r1);
+	cout << "--- COMPARE SURFACES #3 ---\n";
 	r3.compareSurfaces(r2);
+	cout << "--- ROUGH VOLUME ---\n";
 	cout << "Rough volume of " << r3.getName() << ": " << r3.roughVolume() << endl << endl;
 	return 0;
 }
